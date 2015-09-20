2851b5e0d18e1a61704bb5f2933c4dfbb60df5a5
Sakib <isakib@gmail.com>
2015-09-21 00:52:21 +0600

    few more files removed

--- features/step_definitions/user_your_profile.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../../lib/cucumber_helper'
-
-When /^I select multiple checkbox "([^"]*)"$/ do |xpath|
-  page.find(:xpath, xpath).click
-end
--- features/step_definitions/wp_lost_password.rb
+++
@@ -1,9 +0,0 @@
-# require_relative '../../lib/cucumber_helper'
-#
-# Given /^I am retriving with password "(.*)"$/ do |email|
-#   visit "/wp-login.php"
-#   fill_in "Username", :with => email + "email"
-#   click_on "Get New Password"
-# end
-#
-#
